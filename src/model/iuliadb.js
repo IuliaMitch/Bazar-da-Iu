fefecee230d0dd5061e45aaf5d8097913ab5ffc9
IuliaMitch <noipasz@gmail.com>
2022-04-06 13:05:10 -0300

    init table

--- src/model/iuliadb.js
+++ src/model/iuliadb.js
@@ -1,46 +1,48 @@
 import Sequelize from "sequelize";
-import { connection } from '../database/connection.js'
+import { connection } from "../database/connection.js";
 
-export const contos = connection.define('contos', {
+export const contos = connection.define(
+  "contos",
+  {
     id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     nome: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-
-
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
     descricao: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-
-
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
     ano: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-
-
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
     img_reference: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-
-
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
     desc_short: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-    }
-
-}, {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+  },
+  {
     freezeTableName: true,
     createdAt: false,
     updatedAt: false,
-    timestamps: false
-})
+    timestamps: false,
+  }
+);
+
+
+const initTable = async () => {
+    await contos.sync()
+}
+
+
+initTable();
